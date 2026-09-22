71007fadb0e27f9163b65e147041f1908f88c833
BChoudhary <badrc124@gmail.com>
2026-09-22 17:59:10 +0100

    Fixed match result buttons.

--- bjj-elo-app/server/db.js
+++ bjj-elo-app/server/db.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 const { DatabaseSync } = require("node:sqlite");
 
-const dataDir = path.join(__dirname, "..", "data");
+const dataDir = "/data";
 if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
 
 const dbPath = path.join(dataDir, "matrank.db");
